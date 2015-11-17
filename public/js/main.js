d1ca0952c64afc0f13c0f84c0e89e9f08c103e5c
Missy Bur <missybur@gmail.com>
2015-11-17 13:52:12 -0800

    Fix bug issues on index.jade page

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,24 @@ $(document).ready(init);
 
 function init() {
   $('#logout').click(logout);
+  $('#edit').click(editInfo);
+
+
+
+
 }
+
 
 function logout() {
   $.post('/users/logout')
   .done(function(){
     window.location.replace('/');
+  });
+
+  function editInfo(){
+   $('#edit').click(function(){
+    $('div').attr('contenteditable','true');
+})
   });
 }
 
